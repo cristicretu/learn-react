23e3eab6f7022b5774417bc50cb50b50f3658575
Cristi Cretu <cristicretu38@gmail.com>
2021-07-26 09:14:08 +0300

    Adds Favorite Show Button and Toggle - App finished 🚀

--- 01_animelist/src/App.js
+++ 01_animelist/src/App.js
@@ -1,9 +1,15 @@
 import Card from "./components/Card";
 import { useState, useEffect } from "react";
 
+function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+}
+
 function App() {
     const [Query, setQuery] = useState("Naruto");
     const [Results, setResults] = useState([]);
+    const [showFav, setShowFav] = useState(false);
+    const [favorites, setFavorites] = useState([]);
 
     const fetchData = () => {
         const url = `https://api.jikan.moe/v3/search/anime?q=${Query}`;
@@ -11,13 +17,17 @@ function App() {
         fetch(url)
             .then((response) => response.json())
             .then((data) => {
-                console.log(data.results);
+                // console.log(data.results);
                 setResults([...data.results]);
             })
             .catch((error) => {
                 alert("Error while fetching data, check console");
                 console.log(error);
             });
+    };
+
+    const changeFav = () => {
+        setShowFav(!showFav);
     };
 
     useEffect(() => {
@@ -27,24 +37,71 @@ function App() {
     return (
         <div className="flex flex-col items-center my-6 justify-center space-y-8 max-w-4xl mx-auto">
             {/* search box */}
-            <form className="flex" onSubmit={(e) => e.preventDefault()}>
-                <input
-                    onChange={(e) => setQuery(e.target.value)}
-                    className="bg-gray-200 border outline-none border-gray-300 rounded-l-md px-4 py-2"
-                    placeholder="Naruto"
-                />
-                <button
-                    onClick={fetchData}
-                    className="bg-gray-300 rounded-r px-4 py-2 hover:bg-gray-500 hover:text-white transition"
-                >
-                    Search
-                </button>
-            </form>
+            <div className="flex flex-row space-x-8">
+                <form className="flex" onSubmit={(e) => e.preventDefault()}>
+                    <input
+                        onChange={(e) => setQuery(e.target.value)}
+                        className="bg-gray-200 border outline-none border-gray-300 rounded-l-md px-4 py-2"
+                        placeholder="Naruto"
+                    />
+                    <button
+                        onClick={fetchData}
+                        className="bg-gray-300 rounded-r px-4 py-2 hover:bg-gray-500 hover:text-white transition"
+                    >
+                        Search
+                    </button>
+                </form>
+
+                {/* toggle button */}
+                <div className="flex justify-center space-x-2 items-center">
+                    <div
+                        className={classNames(
+                            "relative rounded-full w-12 h-6 transition duration-200 ease-linear",
+                            showFav === true ? "bg-green-400" : "bg-gray-400"
+                        )}
+                    >
+                        <label
+                            htmlFor="toggle"
+                            className={classNames(
+                                "absolute left-0 bg-white border-2 mb-2 w-6 h-6 rounded-full transition transform duration-100 ease-linear cursor-pointer",
+                                showFav === true
+                                    ? "translate-x-full border-green-400"
+                                    : "translate-x-0 border-gray-400"
+                            )}
+                        />
+                        <input
+                            type="checkbox"
+                            id="toggle"
+                            name="toggle"
+                            className="appearance-none w-full h-full active:outline-none focus:outline-none"
+                            onClick={changeFav}
+                        />
+                    </div>
+                    <h1>Show favorites</h1>
+                </div>
+            </div>
             {/* component cards */}
             <div className="flex flex-col space-y-4">
-                {Results.map((data, key) => (
-                    <Card key={key} contents={data} />
-                ))}
+                {showFav &&
+                    favorites.map((data, key) => (
+                        <Card
+                            className="transition"
+                            key={key}
+                            contents={data}
+                            favorites={favorites}
+                            setFavorites={setFavorites}
+                        />
+                    ))}
+                {!showFav &&
+                    Results.map((data, key) => (
+                        <Card
+                            className="transition"
+                            key={key}
+                            contents={data}
+                            favorites={favorites}
+                            setFavorites={setFavorites}
+                        />
+                    ))}
             </div>
         </div>
     );
--- 01_animelist/src/components/Card.js
+++ 01_animelist/src/components/Card.js
@@ -4,13 +4,32 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
 }
 
-function Card({ id, contents: { score, title, synopsis, image_url } }) {
+function Card({
+    id,
+    contents: { score, title, synopsis, image_url },
+    favorites,
+    setFavorites,
+}) {
     let cardStyle =
         score >= 7.5
             ? "bg-green-400"
             : score >= 5.5
             ? "bg-yellow-400"
             : "bg-red-400";
+
+    // https://stackoverflow.com/questions/54676966/push-method-in-react-hooks-usestate
+    const favoriteShow = () => {
+        setFavorites((favorties) => [
+            ...favorites,
+            {
+                score: score,
+                title: title,
+                synopsis: synopsis,
+                image_url: image_url,
+            },
+        ]);
+    };
+
     return (
         <div
             key={id}
@@ -38,6 +57,25 @@ function Card({ id, contents: { score, title, synopsis, image_url } }) {
                         {synopsis}
                     </div>
                 </div>
+            </div>
+
+            <div className="flex-1 text-center py-2 m-2">
+                <button
+                    onClick={favoriteShow}
+                    className="w-12 mt-1 group flex items-center text-gray-800 px-3 py-2 text-base leading-6 font-medium rounded-full transition-all hover:bg-red-800 hover:bg-opacity-70 hover:text-red-500"
+                >
+                    <svg
+                        className="text-center h-7 w-6"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                    >
+                        <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+                    </svg>
+                </button>
             </div>
         </div>
     );
