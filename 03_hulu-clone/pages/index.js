a46f0e0306f723c034d1bb726e8ca46cb9c8c5a6
Cristi Cretu <cristicretu38@gmail.com>
2021-07-30 13:44:09 +0300

    Starter code

---
+++ 03_hulu-clone/pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Create Next App</title>
+            </Head>
+
+            <main>
+                <h1 className="text-deep-orange-500 text-9xl font-extrabold">
+                    Hello World!
+                </h1>
+            </main>
+        </div>
+    );
+}
