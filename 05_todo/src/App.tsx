33b9e81f707b6b84023f3d125cd4bd84a190880e
Cristi Cretu <cristicretu38@gmail.com>
2021-08-12 15:20:07 +0300

    Adds starter code

---
+++ 05_todo/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1 className="text-9xl font-bold text-blue-600">
+                hello typescript
+            </h1>
+        </div>
+    );
+}
+
+export default App;
